ddfb5a06817db4cef10aaaeaaccc9072c69aed75
Guss28 <mailajay3@gmail.com>
2025-05-27 23:20:45 +0530

    adding file

---
+++ main.tf
@@ -0,0 +1,18 @@
+ terraform {
+  required_version = ">= 1.6.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+variable "vm_count" {
+  default = 3
+}
+
+resource "null_resource" "this" {
+  count = var.vm_count
+  triggers = {
+    name = "null-resource-${count.index}"
+  }
+}
