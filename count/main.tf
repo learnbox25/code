6fb0f028684c692a742dbdc51be1cce20749c1bd
Guss28 <mailajay3@gmail.com>
2025-05-27 22:57:48 +0530

    initial commit

--- count/main.tf
+++ count/main.tf
@@ -1,10 +1,17 @@
-variable "number" {
-  type = number
+terraform {
+  required_version = ">= 1.6.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+variable "vm_count" {
   default = 3
 }
 
 resource "null_resource" "this" {
-  count = var.number
+  count = var.vm_count
   triggers = {
     name = "null-resource-${count.index}"
   }
