7f611e62724485a1be63c06930e5e372c6f4f60e
Guss28 <mailajay3@gmail.com>
2025-06-04 23:57:37 +0530

    adding new files

---
+++ ansible/variables.tf
@@ -0,0 +1,14 @@
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "node" {
+  type    = number
+  default = 2
+}
+
+variable "ami" {
+  type    = string
+  default = "ami-084568db4383264d4"
+}
